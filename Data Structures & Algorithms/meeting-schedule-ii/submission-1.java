f500b819731e91e8bcc551eda23541f70fc22331
MoumitaMandal <moumita.nandi@gmail.com>
2026-09-12 09:59:57 -0700

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for(Interval interval: intervals) {
+            if(!heap.isEmpty() && heap.peek() <= interval.start) {
+                heap.poll();
+            }
+            heap.offer(interval.end); //0, 40
+        }
+        return heap.size();
+        
+
+    }
+}
